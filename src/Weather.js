5e4867bd82f1795e5851d5809c7d3c868e4cfcea
VSWild <134617590+victoria-stevenson@users.noreply.github.com>
2023-10-07 17:29:46 +0200

    added search field autofocus

--- src/Weather.js
+++ src/Weather.js
@@ -6,58 +6,55 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="container">
-        <div className="frame">
-          <form id="search">
-            <div className="row">
-              <div className="col-8">
-                <input
-                  type="text"
-                  placeholder="Search City"
-                  className="search-field"
-                  id="search-input"
-                />
-              </div>
-              <div className="col-1">
-                <input type="submit" value="🔍" className="submit-button" />
-              </div>
-              <div className="col-3">
-                <button
-                  className="current-city-button"
-                  id="current-location-btn"
-                >
-                  Current City
-                </button>
-              </div>
-            </div>
-          </form>
-
+      <div className="frame">
+        <form id="search">
           <div className="row">
-            <div className="col-6 city">
-              <h1 id="current-city">Madrid</h1>
-              <p className="today-details">
-                <span className="description" id="weather-description">
-                  {" "}
-                </span>{" "}
-                <br />
-                Humidity: <span id="humidity">45</span>% <br />
-                Wind: <span id="wind-speed">5</span> km/h
-              </p>
+            <div className="col-8">
+              <input
+                type="text"
+                placeholder="Search City"
+                autoFocus="on"
+                className="search-field"
+                id="search-input"
+              />
             </div>
-            <div className="col-6 temp-today-column">
-              <img src="#" alt="" className="icon" id="icon" />
-              <span className="temp-today-font" id="current-temp">
-                30
-              </span>
-              <span className="temp-unit">ºC</span>
-
-              <p className="current-time" id="date-time"></p>
+            <div className="col-1">
+              <input type="submit" value="🔍" className="submit-button" />
+            </div>
+            <div className="col-3">
+              <button className="current-city-button" id="current-location-btn">
+                Current City
+              </button>
             </div>
           </div>
-          <hr />
-          <div id="forecast"></div>
+        </form>
+
+        <div className="row">
+          <div className="col-6 city">
+            <h1 id="current-city">Madrid</h1>
+            <p className="today-details">
+              <span className="description" id="weather-description">
+                {" "}
+              </span>{" "}
+              <br />
+              Humidity: <span id="humidity">45</span>% <br />
+              Wind: <span id="wind-speed">5</span> km/h
+            </p>
+          </div>
+          <div className="col-6 temp-today-column">
+            <img src="#" alt="" className="icon" id="icon" />
+            <span className="temp-today-font" id="current-temp">
+              30
+            </span>
+            <span className="temp-unit">ºC</span>
+
+            <p className="current-time" id="date-time"></p>
+          </div>
         </div>
+        <hr />
+        <div id="forecast"></div>
       </div>
+
       <div className="github-link">
         <small>
           {" "}
